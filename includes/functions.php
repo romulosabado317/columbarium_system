086316d8e9d142e5fd7671023a5fb5a9d5c822f7
romulosabado317 <romulosabado317@gmail.com>
2026-02-10 15:39:10 +0800

    Add email sending functionality using PHPMailer

---
+++ includes/functions.php
@@ -0,0 +1,25 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/SMTP.php';
+function sendEmail($to, $subject, $body){
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'your_email@gmail.com';
+        $mail->Password = 'your_email_password';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->setFrom('your_email@gmail.com', 'Columbarium System');
+        $mail->addAddress($to);
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->send();
+    } catch (Exception $e) { echo "Email could not be sent. Mailer Error: {$mail->ErrorInfo}"; }
+}
+?>
