a3eb25666f339777d27d4af6f9091d1b1de56349
romulosabado317 <romulosabado317@gmail.com>
2026-02-10 16:19:09 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "columbarium_db";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-$conn->set_charset("utf8");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
