3033be7a10befb463c052266e341aa5a399c411b
romulosabado317 <romulosabado317@gmail.com>
2026-02-10 16:02:03 +0800

    Update view_niches.php

--- family/view_niches.php
+++ family/view_niches.php
@@ -1,15 +1,24 @@
 <?php
 include('../includes/db.php');
-session_start();
-if($_SESSION['role'] != 'family') { header('Location: ../index.php'); }
-$niches = $conn->query("SELECT * FROM niches");
+include('../includes/auth.php');
+check_login('family');
+include('../includes/header.php');
+
+$niches = $conn->query("SELECT * FROM niches ORDER BY floor, section, number");
 ?>
+
 <h2>Available Niches</h2>
-<div style='display:flex; flex-wrap:wrap;'>
-<?php while($row = $niches->fetch_assoc()) { ?>
-<div class='niche' id='niche-<?php echo $row['id']; ?>' onclick="window.location='reserve_niche.php?niche_id=<?php echo $row['id']; ?>';">
+<p>Click a niche to reserve it.</p>
+<div style="display:flex; flex-wrap:wrap;">
+<?php while($row = $niches->fetch_assoc()){ 
+    $color = $row['status'] == 'available' ? 'green' : ($row['status'] == 'reserved' ? 'yellow' : 'red');
+?>
+<div class="niche" id="niche-<?php echo $row['id']; ?>" style="background-color:<?php echo $color; ?>;"
+     onclick="<?php if($row['status']=='available'){ echo 'window.location=\'reserve_niche.php?niche_id='.$row['id'].'\''; } ?>">
 <?php echo $row['section'] . $row['number']; ?>
 </div>
 <?php } ?>
 </div>
-<script src='../assets/js/niche_map.js'></script>
+
+<script src="../assets/js/niche_map.js"></script>
+<?php include('../includes/footer.php'); ?>
