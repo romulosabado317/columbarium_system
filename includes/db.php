91efd9d143aead73914a1b6711aee4e08f1cc3df
romulosabado317 <romulosabado317@gmail.com>
2026-02-10 15:35:52 +0800

    Add database connection script in db.php

---
+++ includes/db.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "columbarium_db";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+?>
