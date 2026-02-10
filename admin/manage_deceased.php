6c0a1b0ba6548cbf7e3d1d7c2b148707b074de24
romulosabado317 <romulosabado317@gmail.com>
2026-02-10 15:59:00 +0800

    Update manage_deceased.php

--- admin/manage_deceased.php
+++ admin/manage_deceased.php
@@ -1,11 +1,14 @@
 <?php
 include('../includes/db.php');
-session_start();
-if($_SESSION['role'] != 'admin') { header('Location: ../index.php'); }
+include('../includes/auth.php');
+check_login('admin');
+include('../includes/header.php');
 $deceased = $conn->query("SELECT * FROM deceased");
 ?>
 <h2>Manage Deceased Records</h2>
-<table border='1'><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>DOB</th><th>DOD</th><th>Photo</th></tr>
+<a href='add_deceased.php'>Add Deceased</a>
+<table border='1'>
+<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>DOB</th><th>DOD</th><th>Photo</th><th>Actions</th></tr>
 <?php while($row = $deceased->fetch_assoc()) { ?>
 <tr>
 <td><?php echo $row['id']; ?></td>
@@ -14,5 +17,8 @@ $deceased = $conn->query("SELECT * FROM deceased");
 <td><?php echo $row['dob']; ?></td>
 <td><?php echo $row['dod']; ?></td>
 <td><img src='../assets/photos/<?php echo $row['photo']; ?>' width='50'></td>
+<td><a href='edit_deceased.php?id=<?php echo $row['id']; ?>'>Edit</a></td>
 </tr>
-<?php } ?></table>
+<?php } ?>
+</table>
+<?php include('../includes/footer.php'); ?>
